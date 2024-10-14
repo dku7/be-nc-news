675512a0be910548c5c6afd602362670cb131409
David Ku <dku7@proton.me>
2024-10-14 15:52:37 +0100

    add GET /api/articles to return all articles

--- models/articles.model.js
+++ models/articles.model.js
@@ -11,4 +11,34 @@ function selectArticleById(article_id) {
     });
 }
 
-module.exports = { selectArticleById };
+function selectAllArticles() {
+  return db
+    .query(
+      `
+    SELECT
+      articles.author,
+      articles.title,
+      articles.article_id,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+      CAST(COUNT(comments.comment_id) AS INT) AS comment_count -- force to integer, otherwise returns as string in object
+    FROM articles
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+    GROUP BY
+      articles.author,
+      articles.title,
+      articles.article_id,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url
+    ORDER BY 
+      articles.created_at DESC
+    `
+    )
+    .then((results) => results.rows);
+}
+
+module.exports = { selectArticleById, selectAllArticles };
